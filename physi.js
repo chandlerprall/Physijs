8aa8c08496d43cdee22b3fa1e1de9d2a37e77b07
Chandler Prall <chandler.prall@gmail.com>
2012-04-22 00:34:56 -0600

    Full scene hierarchy support

--- physi.js
+++ physi.js
@@ -7,6 +7,7 @@ window.Physijs = (function() {
 		Eventable, // class to provide simple event methods
 		getObjectId, // returns a unique ID for a Physijs mesh object
 		getEulerXYZFromQuaternion, getQuatertionFromEuler,
+		addObjectChildren,
 		
 		// constants
 		MESSAGE_TYPES = {
@@ -277,40 +278,51 @@ window.Physijs = (function() {
 		this._worker.postMessage({ cmd: cmd, params: params });
 	};
 	
+	addObjectChildren = function( parent, object, offset ) {
+		var i;
+		
+		if ( parent !== object ) {
+			offset.x += object.position.x;
+			offset.y += object.position.y;
+			offset.z += object.position.z;
+		}
+		
+		for ( i = 0; i < object.children.length; i++ ) {
+			if ( object.children[i]._physijs ) {
+				object.children[i]._physijs.offset = {
+					x: object.children[i].position.x + offset.x,
+					y: object.children[i].position.y + offset.y,
+					z: object.children[i].position.z + offset.z
+				};
+				
+				if ( object.children[i].useQuaternion !== true ) {
+					object.children[i].quaternion.copy(getQuatertionFromEuler( object.children[i].rotation.x, object.children[i].rotation.y, object.children[i].rotation.z ));
+				}
+				object.children[i]._physijs.rotation = {
+					x: object.children[i].quaternion.x,
+					y: object.children[i].quaternion.y,
+					z: object.children[i].quaternion.z,
+					w: object.children[i].quaternion.w
+				};
+				
+				parent._physijs.children.push( object.children[i]._physijs );
+			}
+			
+			addObjectChildren( parent, object.children[i], offset.clone() );
+		}
+	};
+	
 	Physijs.Scene.prototype.add = function( object ) {
 		THREE.Mesh.prototype.add.call( this, object );
 		
-		var i;
-		
 		if ( object._physijs ) {
-			
 			object.__dirtyPosition = true;
 			object.__dirtyRotation = true;
 			this._objects[object._physijs.id] = object;
 			
 			if ( object.children.length ) {
 				object._physijs.children = [];
-				for ( i = 0; i < object.children.length; i++ ) {
-					if ( object.children[i]._physijs ) {
-						object.children[i]._physijs.offset = {
-							x: object.children[i].position.x,
-							y: object.children[i].position.y,
-							z: object.children[i].position.z
-						};
-						
-						if ( object.children[i].useQuaternion !== true ) {
-							object.children[i].quaternion.copy(getQuatertionFromEuler( object.children[i].rotation.x, object.children[i].rotation.y, object.children[i].rotation.z ));
-						}
-						object.children[i]._physijs.rotation = {
-							x: object.children[i].quaternion.x,
-							y: object.children[i].quaternion.y,
-							z: object.children[i].quaternion.z,
-							w: object.children[i].quaternion.w
-						};
-
-						object._physijs.children.push( object.children[i]._physijs );
-					}
-				}
+				addObjectChildren( object, object, new THREE.Vector3 );
 			}
 			
 			object.world = this;
