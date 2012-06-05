b94a2555168276a80d816ddd507ac4bad2fbfb4b
Chandler Prall <chandler.prall@gmail.com>
2012-06-05 00:55:13 -0600

    SliderConstraint

--- physi.js
+++ physi.js
@@ -213,12 +213,66 @@ window.Physijs = (function() {
 	Physijs.HingeConstraint.prototype.enableAngularMotor = function( velocity, acceleration ) {
 		this.scene.execute( 'hinge_enableAngularMotor', { constraint: this.id, velocity: velocity, acceleration: acceleration } );
 	};
-	Physijs.HingeConstraint.prototype.enableTargetMotor = function( target ) {
-		this.scene.execute( 'hinge_enableTargetMotor', { constraint: this.id, velocity: target } );
-	};
 	Physijs.HingeConstraint.prototype.disableMotor = function( velocity, acceleration ) {
 		this.scene.execute( 'hinge_disableMotor', { constraint: this.id } );
 	};
+	
+	Physijs.SliderConstraint = function( objecta, objectb, position, axis ) {
+		if ( axis === undefined ) {
+			axis = position;
+			position = objectb;
+			objectb = undefined;
+		}
+		
+		this.type = 'slider';
+		this.id = getObjectId();
+		this.scene = objecta.parent;
+		this.objecta = objecta._physijs.id;
+		this.positiona = convertWorldPositionToObject( position, objecta ).clone();
+		this.axis = axis;
+		
+		if ( objectb ) {
+			this.objectb = objectb._physijs.id;
+			this.positionb = convertWorldPositionToObject( position, objectb ).clone();
+		}
+	};
+	Physijs.SliderConstraint.prototype.getDefinition = function() {
+		return {
+			type: this.type,
+			id: this.id,
+			objecta: this.objecta,
+			objectb: this.objectb,
+			positiona: this.positiona,
+			positionb: this.positionb,
+			axis: this.axis
+		};
+	};
+	Physijs.SliderConstraint.prototype.setLimits = function( lin_lower, lin_upper, ang_lower, ang_upper ) {
+		this.scene.execute( 'slider_setLimits', { constraint: this.id, lin_lower: lin_lower, lin_upper: lin_upper, ang_lower: ang_lower, ang_upper: ang_upper } );
+	};
+	Physijs.SliderConstraint.prototype.setRestitution = function( linear, angular ) {
+		this.scene.execute(
+			'slider_setRestitution',
+			{
+				constraint: this.id,
+				linear: linear,
+				angular: angular
+			}
+		);
+	};
+	Physijs.SliderConstraint.prototype.enableLinearMotor = function( velocity, acceleration) {
+		this.scene.execute( 'slider_enableLinearMotor', { constraint: this.id, velocity: velocity, acceleration: acceleration } );
+	};
+	Physijs.SliderConstraint.prototype.disableLinearMotor = function() {
+		this.scene.execute( 'slider_disableLinearMotor', { constraint: this.id } );
+	};
+	Physijs.SliderConstraint.prototype.enableAngularMotor = function( velocity, acceleration ) {
+		this.scene.execute( 'slider_enableAngularMotor', { constraint: this.id, velocity: velocity, acceleration: acceleration } );
+	};
+	Physijs.SliderConstraint.prototype.disableAngularMotor = function() {
+		this.scene.execute( 'slider_disableAngularMotor', { constraint: this.id } );
+	};
+	
 	
 	
 	// Physijs.Scene
@@ -428,6 +482,21 @@ window.Physijs = (function() {
 						new THREE.MeshNormalMaterial
 					);
 					marker.position.copy( constraint.positiona );
+					this._objects[ constraint.objecta ].add( marker );
+					break;
+				
+				case 'slider':
+					marker = new THREE.Mesh(
+						new THREE.CubeGeometry( 10, 1, 1 ),
+						new THREE.MeshNormalMaterial
+					);
+					marker.position.copy( constraint.positiona );
+					// This rotation isn't right if all three axis are non-0 values
+					marker.rotation.set(
+						constraint.axis.y, // yes, y and
+						constraint.axis.x, // x axis are swapped
+						constraint.axis.z
+					);
 					this._objects[ constraint.objecta ].add( marker );
 					break;
 			}
